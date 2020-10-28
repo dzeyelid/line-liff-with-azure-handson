4a75e24f4629e3018b1b1b2831d9eb56e6d55cc7
Kazumi OHIRA (@dz_) <dzeyelid@gmail.com>
2020-10-28 15:22:16 +0000

    Add Line LIFF SDK

---
+++ src/components/LiffDev.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>LIFF dev</div>
+  <div>{{ lineVersion }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import liff from "@line/liff";
+
+export default defineComponent({
+  name: "LiffDev",
+  data() {
+    return {
+      lineVersion: liff.getLineVersion()
+    };
+  }
+});
+</script>
