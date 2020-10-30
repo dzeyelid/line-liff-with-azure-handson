d2b2605b885b8fc48777fbdf75439a0fb62abbda
Kazumi OHIRA (@dz_) <dzeyelid@gmail.com>
2020-10-30 16:51:27 +0000

    Organize api to get profile

--- api/GetProfile/index.ts
+++ api/GetProfile/index.ts
@@ -6,40 +6,11 @@ type BodyParams = {
   token: string
 }
 
-type LineIdtokenVerifyBody = {
-  id_token: string
-  client_id: string
-}
-
-// type LineRefreshTokenBody = {
-//   grant_type: "refresh_token"
-//   refresh_token: string
-//   client_id: string
-//   client_secret?: string
-// }
-
 type LineAccessTokenVerifyParams = {
   access_token: string
 }
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-  // try {
-  //   const bodyParams = req.body as BodyParams;
-  // const body: LineIdtokenVerifyBody = {
-  //   id_token: bodyParams.token,
-  //   client_id: process.env.LINE_CHANNEL_ID || ''
-  // };
-  // console.log(`body: ${body}`);
-  // const verifyResult = await axios.post(
-  //   "https://api.line.me/oauth2/v2.1/verify",
-  //   querystring.stringify(body),
-  //   {
-  //     headers: {
-  //       "Content-Type": "application/x-www-form-urlencoded"
-  //     },
-  //   }
-  // );
-
   try {
     const bodyParams = req.body as BodyParams;
     const params: LineAccessTokenVerifyParams = {
